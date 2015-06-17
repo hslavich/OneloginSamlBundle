c2b80b162139114b0adb2f53068aa9e48aa5be66
Hernán Slavich <hernan.slavich@gmail.com>
2015-06-17 15:25:19 -0300

    Check SamlToken in logout handler

--- Security/Logout/SamlLogoutHandler.php
+++ Security/Logout/SamlLogoutHandler.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
+use Hslavich\OneloginSamlBundle\Security\Authentication\Token\SamlToken;
 
 class SamlLogoutHandler implements LogoutHandlerInterface
 {
@@ -27,6 +28,10 @@ class SamlLogoutHandler implements LogoutHandlerInterface
      */
     public function logout(Request $request, Response $response, TokenInterface $token)
     {
+        if (!$token instanceof SamlToken) {
+            return;
+        }
+
         try {
             $this->samlAuth->processSLO();
         } catch (\OneLogin_Saml2_Error $e) {
