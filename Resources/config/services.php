332df1d55f1b0b00c15e2ca8ef30ab3af8967bad
Alexander Menshchikov <amenshchikov@gmail.com>
2020-10-12 20:46:12 +0300

    Use FQCN (AbstractArgument will be replaced by abstract_arg)

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -2,18 +2,7 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Hslavich\OneloginSamlBundle\Controller\SamlController;
-use Hslavich\OneloginSamlBundle\EventListener\Security\SamlLogoutListener;
-use Hslavich\OneloginSamlBundle\Security\Authentication\Provider\SamlProvider;
-use Hslavich\OneloginSamlBundle\Security\Authentication\Token\SamlTokenFactory;
-use Hslavich\OneloginSamlBundle\Security\Firewall\SamlListener;
-use Hslavich\OneloginSamlBundle\Security\Http\Authentication\SamlAuthenticationSuccessHandler;
-use Hslavich\OneloginSamlBundle\Security\Http\Authenticator\SamlAuthenticator;
-use Hslavich\OneloginSamlBundle\Security\User\SamlUserProvider;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Argument\AbstractArgument;
-use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
@@ -29,15 +18,15 @@ return static function (ContainerConfigurator $container): void {
         ->args(['%hslavich_onelogin_saml.settings%'])
     ;
 
-    $services->set(SamlController::class);
+    $services->set(\Hslavich\OneloginSamlBundle\Controller\SamlController::class);
 
-    $services->set(SamlListener::class)
+    $services->set(\Hslavich\OneloginSamlBundle\Security\Firewall\SamlListener::class)
         ->parent(service('security.authentication.listener.abstract'))
         ->abstract()
         ->call('setOneLoginAuth', [service(\OneLogin\Saml2\Auth::class)])
     ;
 
-    $services->set(SamlAuthenticator::class)
+    $services->set(\Hslavich\OneloginSamlBundle\Security\Http\Authenticator\SamlAuthenticator::class)
         ->tag('monolog.logger', ['channel' => 'security'])
         ->args([
             /* 0 */ new AbstractArgument('security.http_utils'),
@@ -47,22 +36,22 @@ return static function (ContainerConfigurator $container): void {
             /* 4 */ new AbstractArgument('failure handler'),
             /* 5 */ new AbstractArgument('options'),
             /* 6 */ null,  // user factory
-            /* 7 */ service(EntityManagerInterface::class)->nullOnInvalid(),
-            /* 8 */ service(LoggerInterface::class)->nullOnInvalid(),
+            /* 7 */ service(\Doctrine\ORM\EntityManagerInterface::class)->nullOnInvalid(),
+            /* 8 */ service(\Psr\Log\LoggerInterface::class)->nullOnInvalid(),
         ])
     ;
 
-    $services->set(SamlLogoutListener::class)
-        ->tag('kernel.event_listener', ['event' => LogoutEvent::class])
+    $services->set(\Hslavich\OneloginSamlBundle\EventListener\Security\SamlLogoutListener::class)
+        ->tag('kernel.event_listener', ['event' => \Symfony\Component\Security\Http\Event\LogoutEvent::class])
     ;
 
     $deprecatedAliases = [
-        'hslavich_onelogin_saml.user_provider' => SamlUserProvider::class,
-        'hslavich_onelogin_saml.saml_provider' => SamlProvider::class,
-        'hslavich_onelogin_saml.saml_token_factory' => SamlTokenFactory::class,
-        'hslavich_onelogin_saml.saml_authentication_success_handler' => SamlAuthenticationSuccessHandler::class,
-        'hslavich_onelogin_saml.saml_listener' => SamlListener::class,
-        'hslavich_onelogin_saml.saml_logout_listener' => SamlLogoutListener::class,
+        'hslavich_onelogin_saml.user_provider' => \Hslavich\OneloginSamlBundle\Security\User\SamlUserProvider::class,
+        'hslavich_onelogin_saml.saml_provider' => \Hslavich\OneloginSamlBundle\Security\Authentication\Provider\SamlProvider::class,
+        'hslavich_onelogin_saml.saml_token_factory' => \Hslavich\OneloginSamlBundle\Security\Authentication\Token\SamlTokenFactory::class,
+        'hslavich_onelogin_saml.saml_authentication_success_handler' => \Hslavich\OneloginSamlBundle\Security\Http\Authentication\SamlAuthenticationSuccessHandler::class,
+        'hslavich_onelogin_saml.saml_listener' => \Hslavich\OneloginSamlBundle\Security\Firewall\SamlListener::class,
+        'hslavich_onelogin_saml.saml_logout_listener' => \Hslavich\OneloginSamlBundle\EventListener\Security\SamlLogoutListener::class,
     ];
     foreach ($deprecatedAliases as $alias => $class) {
         $services->alias($alias, $class)->deprecate('hslavich/oneloginsaml-bundle', '2.1', '');
