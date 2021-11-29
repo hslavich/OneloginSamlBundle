d0f2d7e71d3f553a08658819e8441c051b5d2880
Alexander Menshchikov <amenshchikov@gmail.com>
2021-11-29 20:09:28 +0300

    fix tests

--- Tests/Security/User/SamlUserProviderTest.php
+++ Tests/Security/User/SamlUserProviderTest.php
@@ -5,6 +5,7 @@ namespace Hslavich\OneloginSamlBundle\Tests\Security\User;
 use Hslavich\OneloginSamlBundle\Security\User\SamlUserProvider;
 use Hslavich\OneloginSamlBundle\Tests\TestUser;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class SamlUserProviderTest extends TestCase
@@ -20,10 +21,19 @@ class SamlUserProviderTest extends TestCase
 
     public function testRefreshUser(): void
     {
-        $user = $this->createMock(UserInterface::class);
+        $user = new TestUser();
         $provider = $this->getUserProvider();
 
         self::assertSame($user, $provider->refreshUser($user));
+    }
+
+    public function testRefreshUnsupportedUserException(): void
+    {
+        $user = $this->createMock(UserInterface::class);
+        $provider = $this->getUserProvider();
+
+        $this->expectException(UnsupportedUserException::class);
+        $provider->refreshUser($user);
     }
 
     public function testSupportsClass(): void
